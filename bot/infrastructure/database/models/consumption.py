ed89d84c0f8c328707f7a0c35f43bcceba026856
aa08d <alisultanovdzamaludin0@gmail.com>
2025-09-30 14:29:34 +0300

    feat: Consumption model

---
+++ bot/infrastructure/database/models/consumption.py
@@ -0,0 +1,14 @@
+from uuid6 import uuid7
+from sqlalchemy import Column, UUID, Float, Date, String
+
+from .base import Base
+
+
+class Consumption(Base):
+    __tablename__ = 'consumptions'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid7())
+    previous = Column(Float(asdecimal=True))
+    current = Column(Float(asdecimal=True), nullable=True)
+    photo_url = Column(String, nullable=True)
+    date = Column(Date)
